993edb0aa0cfb94f48a50362318ac6c83d871942
Alex Aldrick <alex@alexaldrick.com>
2021-01-19 16:14:51 +0100

    keybinds for scratch and vimwikiindex updated

--- config.h
+++ config.h
@@ -213,8 +213,8 @@ static Key keys[] = {
 	/* V is automatically bound above in STACKKEYS */
 	{ MODKEY,			XK_b,		togglebar,	{0} },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
-	{ MODKEY,			XK_v,		spawn,		SHCMD(TERMINAL " -e nvim ~/dox/VimWiki/Scratch.md") },
-	{ MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
+	{ MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD(TERMINAL " -e nvim ~/dox/VimWiki/Scratch.md") },
+	{ MODKEY,			XK_v,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
 	{ MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
